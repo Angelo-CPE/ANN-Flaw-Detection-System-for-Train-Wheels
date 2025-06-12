24593476d2299f11ed6704f708cb2a4fb7d38fe6
Angelo-CPE <angelo.dscpl@gmail.com>
2025-06-12 20:02:46 +0800

    updated jwt

--- wheel-inspection-backend/server.js
+++ wheel-inspection-backend/server.js
@@ -36,7 +36,7 @@
 
   const PORT = process.env.PORT || 5000;
   const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://pdTeam39:t39@cluster0.khfnesv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-  const JWT_SECRET = process.env.JWT_SECRET || crypto.randomBytes(32).toString('hex');
+  const JWT_SECRET = process.env.JWT_SECRET;
   const EMAIL_USER = process.env.EMAIL_USER;
   const EMAIL_PASS = process.env.EMAIL_PASS;
 
