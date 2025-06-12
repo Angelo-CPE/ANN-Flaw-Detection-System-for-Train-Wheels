ca87149ebedfba3bf530d002e45979c95d3f895b
Angelo-CPE <angelo.dscpl@gmail.com>
2025-06-12 20:20:34 +0800

    updated password validation

--- wheel-inspection-backend/server.js
+++ wheel-inspection-backend/server.js
@@ -77,7 +77,7 @@
       select: false,
       validate: {
         validator: function(v) {
-          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$/.test(v);
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(v);
         },
         message: props => 
           'Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character'
