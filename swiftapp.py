ff3284f81e571e611f6423e0964eb87c0a872974
Angelo <angelo.dscpl@gmail.com>
2025-06-30 06:55:35 +0800

    confidence_value

--- swiftapp.py
+++ swiftapp.py
@@ -464,7 +464,7 @@ class CameraThread(QThread):
 
             confidence_value = confidence.item()
 
-            if confidence_value < 0.7:
+            if confidence_value < 0.75:
                 status = "UNKNOWN"
                 recommendation = "Please Reposition and Retest"
             elif predicted.item() == 1:
