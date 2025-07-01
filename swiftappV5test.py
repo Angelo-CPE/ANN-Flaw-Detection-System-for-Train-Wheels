84fe8e1edf1482b882d83df63cdd66cee7c6e65d
Angelo <angelo.dscpl@gmail.com>
2025-07-02 06:08:14 +0800

    changed Logo

--- swiftappV5test.py
+++ swiftappV5test.py
@@ -645,7 +645,7 @@ class HomePage(QWidget):
         # Logo
         self.logo_label = QLabel()
         self.logo_label.setAlignment(Qt.AlignCenter)
-        logo_pixmap = QPixmap('logoV3.png')
+        logo_pixmap = QPixmap('logoV.png')
         if not logo_pixmap.isNull():
             self.logo_label.setPixmap(logo_pixmap.scaledToHeight(100, Qt.SmoothTransformation))
         self.layout.addWidget(self.logo_label)
